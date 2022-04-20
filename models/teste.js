a673602ffd010f7bfa1325bab612016c23fee342
Diego <>
2022-04-20 03:07:35 -0300

    Desenvolvendo regras de validação

--- models/teste.js
+++ models/teste.js
@@ -1,19 +1,78 @@
-[
+inputData = {
+    "Fare": 4,
+    "OpertorCode": 3,
+    "IncludeData": "2022-12-20T04:04:08.264Z"   
+}
+
+lista = [
 	{
 		"Fare": 4,
-		"Opertor-Code": 3,
-		"Include-Data": "2022-04-20T04:04:08.264Z"
+		"OpertorCode": 3,
+		"IncludeData": "2022-04-20T04:04:08.264Z"
 	},
 	{
 		"Fare": 1,
-		"Opertor-Code": 3,
-		"Include-Data": "2022-04-20T03:40:18.401Z"
+		"OpertorCode": 2,
+		"IncludeData": "2022-04-20T03:40:18.401Z"
 	},
     {
 		"Fare": 1,
-		"Opertor-Code": 3,
-		"Include-Data": "2022-04-20T03:40:18.401Z"
+		"OpertorCode": 1,
+		"IncludeData": "2022-04-20T03:40:18.401Z"
 	}
 ]
 
-filtro
+function validateMouth (dados,inputData){
+    let dataAtual = new Date(inputData)
+    // console.log(new Date(dados.IncludeData).getTime())
+    let includeData = new Date(dados[0].IncludeData).getTime()
+    
+    let timeDiff = Math.abs(dataAtual.getTime()-includeData);
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    let diasPorMes = diffDays/30
+    let mesTratado = Math.round(diasPorMes)
+    
+    if(mesTratado>=6){
+        return false
+    }else{
+        return true
+    }
+}
+
+
+
+function validandoEmpresa(array,inputData){
+let listaEmpresas = []
+return array.filter((obj) =>{
+    if(obj.OpertorCode == inputData.OpertorCode){  
+       listaEmpresas.push(obj)
+    return listaEmpresas
+    }
+})
+
+
+}
+
+function analyseOperatorCode(lista,inputData){
+operatorDuplicity = validandoEmpresa(lista,inputData)
+    if(operatorDuplicity.length<0){
+        console.log('dados não repetidos')
+    }
+    else{
+        console.log('dados repetidos')
+        console.log(validateMouth (lista,inputData))
+        //if(false){
+        // console.log("seu registro nao pode ser realizado pois já existe outro")
+        // }else{
+////realizar registro
+        // }
+    }
+}
+
+
+
+analyseOperatorCode(lista,inputData)
+
+
+////////validação de datas esta errado
+
